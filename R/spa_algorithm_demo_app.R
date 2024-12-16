6ae7723b12705260def62c2f1b521915cb216440
stephaneghozzi <hello@stephaneghozzi.com>
2024-12-16 21:20:29 +0000

    displays NA's in score table

--- R/spa_algorithm_demo_app.R
+++ R/spa_algorithm_demo_app.R
@@ -1,5 +1,7 @@
 spa_algorithm_demo_app <- function() {
 
+  options(htmlwidgets.TOJSON_ARGS = list(na = 'string'))
+
   shiny::shinyApp(
     ui = spa_algorithm_demo_ui,
     server = spa_algorithm_demo_server
